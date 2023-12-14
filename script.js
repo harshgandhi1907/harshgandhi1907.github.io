479590f98a25efd9224f5db41ca4e1031785308c
harshgandhi1907 <gandhiharsh1907@gmail.com>
2023-12-14 17:58:11 +0530

    .

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ document.addEventListener("DOMContentLoaded", () => {
         let totalExpense = 0;
         // const consumer_key = "3MVG95mg0lk4bath_h7i4xZH5uzPYZ_0FZuNbtNGb2eyGFnf3SlckXUQtOAQ56jluM1ChiUBLbI_RTXPbgPF3";
         // const consumer_secret =  "38C1EF975BA58FBF9FD2C5DA0AC44264B3717D90800101CAD79CA6825715B3C8";
-        const salesforceEndpoint = "expensetrackerportal-dev-ed.develop.my.salesforce.com/services/data/v58.0/sobjects/Expense__c";
+        const salesforceEndpoint = "https://expensetrackerportal-dev-ed.develop.my.salesforce.com";
         async function addExpenseToSalesforce(name, amount) {
             try {
                 const accessToken = "00D5h0000093stB!ARMAQJFeFELpvLe9iZHCQqoIekwTI5_1Fl5lZn8aTVP9QxKlBIQRIug_FXsicyQdJK7Mf5pALoLUBPVCZToAhYW7nt9j8id0";
