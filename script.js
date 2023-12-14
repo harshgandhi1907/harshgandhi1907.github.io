63ea8f07923ffcc4f44b2372a95a1ecf8425de3c
jaival2512 <choksijaival@gmail.com>
2023-12-14 16:27:22 +0530

    jaival14/12

--- script.js
+++ script.js
@@ -50,7 +50,6 @@ document.addEventListener("DOMContentLoaded", () => {
                 if (name && amount) {
                     expenses.push({ name, amount });
                     totalExpense += amount;
-                    await addExpenseToSalesforce(name, amount); // Add the expense to Salesforce
                     updateUI();
                     expenseForm.reset();
                     toggleExpenseListVisibility();
