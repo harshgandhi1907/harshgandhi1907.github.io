871b9e0a024b444c3f78ac48db367be8d40c59c1
harshgandhi1907 <gandhiharsh1907@gmail.com>
2023-12-14 12:27:35 +0530

    revert api callout

--- ExpenseTracker/script.js
+++ ExpenseTracker/script.js
@@ -21,36 +21,6 @@ document.addEventListener("DOMContentLoaded", () => {
                     totalExpense += amount;
                     updateUI();
                     expenseForm.reset();
-
-                    // Rest API callout
-                    // var data = {
-                    //     Name: (Math.random()).toString(),
-                    //     Expense_Name__c: name,
-                    //     Expense_Amount__c: amount
-                    // };
-                    // // consumer key = 3MVG9pRzvMkjMb6naRM353RGyfO_sA0RSipQL3qs0I8rba2pDr1arq8GaEy5PnjyOvuR8Vkz1MYORLHlYfs6s
-                    // // consumer secret =  460ADD94008F060A4F3E58FBCE32EC0122A3929E335D703E7FBF72AE5A3E4215
-                    // var access_token = '6Cel800D5j00000CfpEr8885j000000hFF1sx4PcANYC4BmtyMxqoaGR5lMuVoE7QhIRYMleURFncbZPYUrDYuwRNtGdRIpQykOd4hj7V0O';
-                    // if(access_token != ''){
-                    //     var xhr = new XMLHttpRequest();
-                    //     xhr.open('POST', 'https://harsh-2b-dev-ed.develop.lightning.force.com/services/data/v58.0/sobjects/Expense__c/', true);
-                    //     xhr.setRequestHeader('Content-Type', 'application/json');
-                    //     xhr.setRequestHeader('Authorization', 'Bearer ' + access_token);
-                        
-                    //     xhr.onreadystatechange = function() {
-                    //         if (xhr.readyState === 4 && xhr.status === 200) {
-                    //             console.log('Data stored successfully');
-                    //         } else {
-                    //             console.error('Error storing data');
-                    //         }
-                    //     };
-                    //     console.log(xhr);
-                        
-                    //     xhr.send(JSON.stringify(data));
-                    //     toggleExpenseListVisibility();
-                    // } else{
-                    //     alert('Something went wrong !! Please try again');
-                    // }
                 }
             } catch (error) {
                 console.log('error in submit click ==> ' + error);
