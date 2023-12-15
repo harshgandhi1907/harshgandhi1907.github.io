6822cf0567281424125c7f715d29e17c59aeb99d
harshgandhi1907 <gandhiharsh1907@gmail.com>
2023-12-15 12:35:44 +0530

    js update

--- index.js
+++ index.js
@@ -22,34 +22,5 @@ links.forEach(link => {
   link.addEventListener("click", e => {
     e.preventDefault(); //preventing form submit
     forms.classList.toggle("show-signup");
-  })
-})
-link.addEventListener("click", e => {
-  e.preventDefault(); //preventing form submit
-  forms.classList.toggle("show-signup");
-})
-
-eyeIcon.addEventListener("click", () => {
-  let pwFields = eyeIcon.parentElement.parentElement.querySelectorAll(".password");
-
-  pwFields.forEach(password => {
-    if (password.type === "password") {
-      password.type = "text";
-      eyeIcon.classList.replace("bx-hide", "bx-show");
-      return;
-    }
-    password.type = "password";
-    eyeIcon.classList.replace("bx-show", "bx-hide");
   });
 });
-
-links.forEach(link => {
-  link.addEventListener("click", e => {
-    e.preventDefault(); //preventing form submit
-    forms.classList.toggle("show-signup");
-  });
-});
-
-document.getElementById("loginButton").onclick = function () {
-  window.location.href = "/home.html";
-};
