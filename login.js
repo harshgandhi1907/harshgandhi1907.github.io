b5cf42083d5f93f9b81a621049c58e1dce441263
jaival2512 <choksijaival@gmail.com>
2023-12-15 12:13:35 +0530

    jaival15/12

--- login.js
+++ login.js
@@ -1,36 +1,30 @@
 const forms = document.querySelector(".forms"),
-pwShowHide = document.querySelectorAll(".eye-icon"),
-links = document.querySelectorAll(".link");
+    pwShowHide = document.querySelectorAll(".eye-icon"),
+    links = document.querySelectorAll(".link");
 
 pwShowHide.forEach(eyeIcon => {
-  eyeIcon.addEventListener("click", () => {
-    let pwFields = eyeIcon.parentElement.parentElement.querySelectorAll(".password");
-    
-    pwFields.forEach(password => {
-        if(password.type === "password"){
-            password.type = "text";
-            eyeIcon.classList.replace("bx-hide", "bx-show");
-            return;
-        }
-        password.type = "password";
-        eyeIcon.classList.replace("bx-show", "bx-hide");
+    eyeIcon.addEventListener("click", () => {
+        let pwFields = eyeIcon.parentElement.parentElement.querySelectorAll(".password");
+
+        pwFields.forEach(password => {
+            if (password.type === "password") {
+                password.type = "text";
+                eyeIcon.classList.replace("bx-hide", "bx-show");
+                return;
+            }
+            password.type = "password";
+            eyeIcon.classList.replace("bx-show", "bx-hide");
+        })
     })
-  })
-})      
+})
 
 links.forEach(link => {
-<<<<<<< HEAD
+    link.addEventListener("click", e => {
+        e.preventDefault(); //preventing form submit
+        forms.classList.toggle("show-signup");
+    })
+})
 link.addEventListener("click", e => {
- e.preventDefault(); //preventing form submit
- forms.classList.toggle("show-signup");
-})
-})
-=======
-  link.addEventListener("click", e => {
     e.preventDefault(); //preventing form submit
     forms.classList.toggle("show-signup");
-  })
 })
-
-
->>>>>>> f0be7e8f1505e39cb83be1a1080c3b077f75a607
