39a80da7a00851f523c97ed3171d3592125ffb34
jaival2512 <choksijaival@gmail.com>
2023-12-15 12:34:21 +0530

    jaival15/12

--- index.js
+++ index.js
@@ -1,49 +1,47 @@
 const forms = document.querySelector(".forms"),
-    pwShowHide = document.querySelectorAll(".eye-icon"),
-    links = document.querySelectorAll(".link");
+  pwShowHide = document.querySelectorAll(".eye-icon"),
+  links = document.querySelectorAll(".link");
 
 pwShowHide.forEach(eyeIcon => {
-<<<<<<< HEAD:login.js
-    eyeIcon.addEventListener("click", () => {
-        let pwFields = eyeIcon.parentElement.parentElement.querySelectorAll(".password");
+  eyeIcon.addEventListener("click", () => {
+    let pwFields = eyeIcon.parentElement.parentElement.querySelectorAll(".password");
 
-        pwFields.forEach(password => {
-            if (password.type === "password") {
-                password.type = "text";
-                eyeIcon.classList.replace("bx-hide", "bx-show");
-                return;
-            }
-            password.type = "password";
-            eyeIcon.classList.replace("bx-show", "bx-hide");
-        })
+    pwFields.forEach(password => {
+      if (password.type === "password") {
+        password.type = "text";
+        eyeIcon.classList.replace("bx-hide", "bx-show");
+        return;
+      }
+      password.type = "password";
+      eyeIcon.classList.replace("bx-show", "bx-hide");
     })
+  })
 })
 
 links.forEach(link => {
-    link.addEventListener("click", e => {
-        e.preventDefault(); //preventing form submit
-        forms.classList.toggle("show-signup");
-    })
-})
-link.addEventListener("click", e => {
+  link.addEventListener("click", e => {
     e.preventDefault(); //preventing form submit
     forms.classList.toggle("show-signup");
+  })
 })
-=======
-  eyeIcon.addEventListener("click", () => {
-    let pwFields = eyeIcon.parentElement.parentElement.querySelectorAll(".password");
-    
-    pwFields.forEach(password => {
-        if(password.type === "password"){
-            password.type = "text";
-            eyeIcon.classList.replace("bx-hide", "bx-show");
-            return;
-        }
-        password.type = "password";
-        eyeIcon.classList.replace("bx-show", "bx-hide");
-    });
+link.addEventListener("click", e => {
+  e.preventDefault(); //preventing form submit
+  forms.classList.toggle("show-signup");
+})
+
+eyeIcon.addEventListener("click", () => {
+  let pwFields = eyeIcon.parentElement.parentElement.querySelectorAll(".password");
+
+  pwFields.forEach(password => {
+    if (password.type === "password") {
+      password.type = "text";
+      eyeIcon.classList.replace("bx-hide", "bx-show");
+      return;
+    }
+    password.type = "password";
+    eyeIcon.classList.replace("bx-show", "bx-hide");
   });
-});      
+});
 
 links.forEach(link => {
   link.addEventListener("click", e => {
@@ -52,7 +50,6 @@ links.forEach(link => {
   });
 });
 
-document.getElementById("loginButton").onclick = function() {
+document.getElementById("loginButton").onclick = function () {
   window.location.href = "/home.html";
 };
->>>>>>> b0baa5a22ea64fa7e4c611c05730c14ca7495293:index.js
